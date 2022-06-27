feb4c3467e9cd4dc0b992186a40235bf3fcc5193
Henry <hungcv1992@gmail.com>
2022-06-27 21:39:40 +0700

    fixed android

--- android/app/src/main/java/com/pitech_jogtoearn_mobile/MainActivity.java
+++ android/app/src/main/java/com/pitech_jogtoearn_mobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pitech_jogtoearn_mobile;
 
+import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.NavigationActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
